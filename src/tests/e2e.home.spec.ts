968841bca95fa2d539ed771f2b89ad432969c499
champ <techmr.singh@gmail.com>
2025-08-20 23:28:49 -0700

    ci: fast smoke on PRs, full on main

--- src/tests/e2e.home.spec.ts
+++ src/tests/e2e.home.spec.ts
@@ -1,7 +1,7 @@
 import { test } from '@playwright/test';
 import { HomePage } from '../pages/home.page';
 
-test.describe('@e2e Home', () => {
+test.describe('@e2e @smoke Home', () => {
   test('navigate to getting started via POM', async ({ page }) => {
     const home = new HomePage(page);
     await page.goto('https://playwright.dev/');   
